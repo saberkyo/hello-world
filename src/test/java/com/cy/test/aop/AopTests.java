b837d6ca1228dd72f99070367a9e3955bffaab55
saberkyo <1596550812@qq.com>
2019-09-12 08:54:44 +0800

    test

---
+++ src/test/java/com/cy/test/aop/AopTests.java
@@ -0,0 +1,27 @@
+package com.cy.test.aop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cy.pj.sys.service.SysUserService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AopTests {
+	@Autowired
+	private ApplicationContext ctx;
+
+	// ClassPathXmlApplicationContext
+	// AnnotationConfigApplicationContext
+	@Test
+	public void testSysUserService() {
+		SysUserService userService = ctx.getBean("sysUserServiceImpl", SysUserService.class);
+//		 PageObject<SysUserDeptVo> po=
+		userService.findPageObjects("admin", 1);
+	}
+
+}
